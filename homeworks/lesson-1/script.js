7489b001121c1c35a4ca6d4e781ac05cecbed514
satikpro <saturnpro73@gmail.com>
2021-04-02 21:55:01 +0300

    first commit

---
+++ homeworks/lesson-1/script.js
@@ -0,0 +1,24 @@
+let boolean = true;
+console.log( typeof itsBoolean);
+let str = "true";
+console.log( typeof itsString);
+let num  = 123;
+console.log( typeof itsNumber);
+let txt = Symbol("Привет") 
+console.log(typeof itsSymbol);
+
+
+
+
+
+
+
+let a1 = 5 + 3;
+let a2 = 5 - 3;
+let a3 = 5 * 3;
+let a4 = 5 / 3;
+let a5 = 5 % 3;
+let a6 = 3 % 5;
+let a7 = 5 + '3'; 
+let a8 = '5' - 3;
+let a9 = 75 + 'кг';
