5cef573e6351c0b3730f682037ee7bdcd6a2f6c7
satikpro <saturnpro73@gmail.com>
2021-04-02 17:08:28 +0300

    first commit

---
+++ homeworks/lesson-1/Glinsky_Ilya/script.js
@@ -0,0 +1,24 @@
+let itsBoolean = true;
+console.log( typeof itsBoolean);
+let itsString = "true";
+console.log( typeof itsString);
+let itsNumber  = 123;
+console.log( typeof itsNumber);
+let itsSymbol = Symbol("Привет") 
+console.log(typeof itsSymbol);
+
+
+
+
+
+
+
+let a1 = 5 + 3;
+let a2 = 5 - 3;
+let a3 = 5 * 3;
+let a4 = 5 / 3;
+let a5 = 5 % 3;
+let a6 = 3 % 5;
+let a7 = 5 + '3'; 
+let a8 = '5' - 3;
+let a9 = 75 + 'кг';
