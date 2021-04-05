868f5bee87a0adca319255cf36b229e399842ada
Suprun Masha <suprunm337@gmail.com>
2021-04-05 22:01:47 +0300

    add  new files

---
+++ homeworks/lesson-2/Masha_Suprun/script.js
@@ -0,0 +1,30 @@
+let test = true;
+if (test === true) {
+    console.log("correctly")
+}
+else {
+    console.log("incorrectly")
+}
+
+let a = 1;
+let b = 3;
+if(a <= 1 && b >= 3){
+    console.log(a + b)
+}
+else{
+    console.log(b - a)
+}
+
+let num = 2;
+switch(num){
+    case(1): console.log(result = "winter")
+      break;
+    case(2): console.log(result = "spring")
+      break;
+    case(3): console.log(result = "summer")
+      break;
+    case(4): console.log(result = "autumn")
+      break;
+    default: console.log('error')
+      break;
+}
